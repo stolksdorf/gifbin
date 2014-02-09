17151ff8497eee103dcb8492cb4478923a10cf4c
Scott <scott.tolksdorf@gmail.com>
2014-02-08 21:56:50 -0500

    Made category page a bit cleaner, added link counts to downloads

--- public/js/CategoryCardView.js
+++ public/js/CategoryCardView.js
@@ -1,14 +1,12 @@
 CategoryView = xo.view.extend({
 	schematic : DOM.a({class:'category'},
-		DOM.div({class:'category__gifContainer', 'xo-element' : 'gifContainer'}),
-		//DOM.div({class:'category__title'},
-			DOM.div({class:'category__name', 'xo-element' : 'name'}),
-			DOM.div({class:'category__stats'},
-				DOM.span({'xo-element' : 'gifCount'}),
-				" gifs - ",
-				DOM.span({'xo-element' : 'linkCount'}),
-				" links")
-		//)
+		DOM.img({class:'category__gifContainer', 'xo-element' : 'gifContainer'}),
+		DOM.div({class:'category__name', 'xo-element' : 'name'}),
+		DOM.div({class:'category__stats'},
+			DOM.span({'xo-element' : 'gifCount'}),
+			" gifs - ",
+			DOM.span({'xo-element' : 'linkCount'}),
+			" links")
 	),
 
 	render : function(){
@@ -17,49 +15,9 @@ CategoryView = xo.view.extend({
 		this.dom.gifCount.text(this.model.numGifs);
 		this.dom.linkCount.text(this.model.totalLinks);
 
-		this.gifView = CardView.create(this.model.gifModel).appendTo(this.dom.gifContainer);
+		//this.gifView = CardView.create(this.model.gifModel).appendTo(this.dom.gifContainer);
+		this.dom.gifContainer.attr('src', this.model.gifModel.staticLink);
 		return this;
 	},
 
 });
-
-
-/*
-
-css.render({
-	'.category' : {
-		'display': 'inline-block',
-		'vertical-align' : 'top',
-		'background-color': '#ECF0F1',
-		'cursor': 'pointer',
-		'text-align': 'center',
-		'padding': '20px',
-		'padding-top': '10px',
-		'margin': '5px',
-		'width': '140px',
-		'text-decoration': 'none',
-		'transition': '0.25s',
-		'-webkit-transition': '0.25s',
-	},
-		'.category__title' : {
-			'margin-bottom': '7px',
-
-			'font-size': '1.5em',
-			'color': '#2C3E50',
-		},
-		'.category__image' : {
-			'max-height': '150px',
-			'max-width': '140px',
-		},
-
-
-		'.category:hover' : {
-			'background-color': '#E1E5E5',
-		},
-
-		'.category:first-child' : {
-			'margin-left': '0px',
-		}
-})
-
-*/
