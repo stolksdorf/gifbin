8c9537ad9362946b7f8e7408f5b22110adbddfc4
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2015-07-24 09:33:31 -0400

    Stubbed out alittle info box for the search bar

--- client/gifbin/edit/edit.jsx
+++ client/gifbin/edit/edit.jsx
@@ -1,32 +1,14 @@
 /** @jsx React.DOM */
 var React = require('react');
 var _ = require('underscore');
-
-//var GifStore = require('gifbin/gifstore');
-
 var GifForm = require('gifbin/gifForm/gifForm.jsx');
 
-
 var Edit = React.createClass({
-
-/*
-	getInitialState: function() {
-		return {
-			gif : this.props.gif
-		};
-	},
-*/
 	componentDidMount: function() {
 		document.title = 'gifbin.edit';
 	},
-
-
 	render : function(){
 		var self = this;
-
-		console.log(this.props.gif);
-
-
 		return(
 			<div className='edit'>
 				<GifForm gif={this.props.gif} />
