b9d489d72368b2f79f60453a0cebf60bac52fa64
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2015-08-21 23:50:29 -0400

    Finally fixed up the admin routes

--- client/admin/admin.jsx
+++ client/admin/admin.jsx
@@ -25,13 +25,57 @@ var Admin = React.createClass({
 		)
 	},
 
+	downloadFile : function(contents, filename){
+		document.body.innerHTML += '<a id="downloadAnchorElem" style="display:none"></a>';
+		var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(contents);
+		var dlAnchorElem = document.getElementById('downloadAnchorElem');
+		dlAnchorElem.setAttribute("href", dataStr);
+		dlAnchorElem.setAttribute("download", filename);
+		dlAnchorElem.click();
+		document.body.removeChild(dlAnchorElem);
+	},
+
+	exportDatabase : function(){
+		var self = this;
+		request
+			.get('/api/admin/export')
+			.query({admin_key : this.props.ADMIN_KEY})
+			.end(function(err, res){
+				console.log(err, res);
+				var dateString = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+				var filename = 'gifbin-dump-' + dateString + '.json';
+				self.downloadFile(JSON.stringify(JSON.parse(res.text), null, '\t'), filename);
+			});
+	},
+
 	dropDatabase : function(){
 		request
 			.get('/api/admin/drop')
-			.query({admin_key : this.props.ADMIN_KEY + 1})
+			.query({admin_key : this.props.ADMIN_KEY})
 			.end(function(err, res){
-				console.log(err, res);
-			})
+				if(!err) alert('All gifs removed.');
+				window.location.reload();
+			});
+	},
+
+	importDatabase : function(e){
+		var self = this;
+		this.fileReader.onload = function() {
+			var text = self.fileReader.result;
+			request
+				.post('/api/admin/import')
+				.query({admin_key : self.props.ADMIN_KEY})
+				.send(JSON.parse(self.fileReader.result))
+				.end(function(err, res){
+					if(!err) alert('Gifs imported.');
+					window.location.reload();
+				});
+		}
+		this.fileReader.readAsText(this.refs.fileUpload.getDOMNode().files[0]);
+	},
+
+	componentDidMount: function() {
+		this.fileReader = new FileReader();
 	},
 
 	render : function(){
@@ -48,15 +92,18 @@ var Admin = React.createClass({
 				<div className='container'>
 
 					<div className='buttonContainer'>
-						<button className='exportButton'>
+						<button className='exportButton' onClick={this.exportDatabase}>
 							<i className='fa fa-download' />
 							Export Database
 						</button>
 
-						<button className='importButton'>
-							<i className='fa fa-cloud-upload' />
-							Import Database
-						</button>
+						<div className='importContainer'>
+							<button className='importButton'>
+								<i className='fa fa-cloud-upload' />
+								Import Database
+							</button>
+							<input type="file" onChange={this.importDatabase} ref='fileUpload' />
+						</div>
 
 						<button className='trashButton' onClick={this.dropDatabase}>
 							<i className='fa fa-trash' />
